766d47922f5144b973a9e5a24a5ec276e5db1d7d
Max Fr <fruehaufmaximilian@gmail.com>
2019-07-13 16:13:22 +0200

    asdfasdf

--- py/train_variable.py
+++ py/train_variable.py
@@ -125,7 +125,7 @@ def block_process(bins, args):
         path = bin_path(b)
         print(path)
         if not os.path.isfile(path):
-            res = generate_feature(b, bin_dir, debug_dir, bap_dir)
+            res = list(generate_feature(b, bin_dir, debug_dir, bap_dir))
             print('analysed binary {} writing to {}'.format(b, path))
             with gzip.open(path, 'wb') as f:
                 pickle.dump(res, f)
